4f2277b2240a563ac9690fa76e86de2fb2dc788c
SashaPanasiuk <sasha.panasiuk22865466@gmail.com>
2020-05-09 14:28:30 +0300

    Upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float WalkSpeed = 3f;
+	public float JumpForce = 5f;
+	public float FlyingMovementSpeed = 3f;
+
+	private MoveState movestate = MoveState.Idle;
+	private Direction direction = Direction.Right;
+
+
+	private Transform transform;
+	private Rigidbody2D rigidbody;
+	private Animator animator;
+
+	private int JumpCounter = 0;
+	private float WalkCounter = 0;
+	private float WalkCouldown = 0.1f;
+
+
+	public void MoveLeft(){
+		if (movestate != MoveState.Jump) {
+			
+			movestate = MoveState.Move;
+			if (direction == Direction.Right) {
+				transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+				direction = Direction.Left;
+
+			}
+
+			WalkCounter = WalkCouldown;
+			animator.Play ("Walk");	
+		}else{
+			if (direction == Direction.Right) {
+				transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+				direction = Direction.Left;
+
+				movestate = MoveState.Jump;
+
+
+			}
+			WalkCounter = WalkCouldown;
+		} 
+	}
+	public void MoveRight(){
+		if(movestate != MoveState.Jump){
+
+			movestate = MoveState.Move;
+			if(direction == Direction.Left){
+				transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.z);
+				direction = Direction.Right;
+
+			}
+
+			WalkCounter = WalkCouldown;
+			animator.Play("Walk");	
+		}else{
+			if (direction == Direction.Left) {
+				transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+				direction = Direction.Right;
+
+				movestate = MoveState.Jump;
+			
+
+			}
+			WalkCounter = WalkCouldown;
+		}  
+	}
+	public void Jump(){
+		JumpCounter++;
+	
+			if (JumpCounter <= 1) {
+				rigidbody.velocity = (Vector2.up * JumpForce * Time.deltaTime * 10);
+				movestate = MoveState.Jump;
+
+
+				animator.Play ("Jump1");
+
+			} else {
+				if (rigidbody.velocity == Vector2.zero) {
+					JumpCounter = 0;
+					rigidbody.velocity = (Vector2.up * JumpForce * Time.deltaTime * 10);
+					movestate = MoveState.Jump;
+					
+
+					animator.Play ("Jump1");
+				}
+			}
+
+	}
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform>();
+		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (movestate);
+		 if (movestate == MoveState.Move) {
+			
+			rigidbody.velocity = ((direction == Direction.Left ? -Vector2.right : Vector2.right) * WalkSpeed * Time.deltaTime);
+
+				
+
+
+			WalkCounter -= Time.deltaTime;
+			if (WalkCounter <= 0) {
+				movestate = MoveState.Idle;
+				animator.Play ("Idle");
+			}
+		} else if (movestate == MoveState.Jump) {
+			
+
+			WalkCounter -= Time.deltaTime;
+			if (WalkCounter >= 0) {
+				rigidbody.velocity += ((direction == Direction.Left ? -Vector2.right : Vector2.right) * WalkSpeed/10 * Time.deltaTime);
+			}
+			if (rigidbody.velocity.y == 0) {
+				
+				WalkCounter = 0;
+				movestate = MoveState.Idle;
+				animator.Play ("Idle");
+
+			}
+		}
+		
+	}
+	enum MoveState{
+		Jump,
+		Move,
+		Idle
+	}
+	enum Direction{
+		Right,
+		Left
+	}
+
+}
---
+++ Assets/Scripts/PlayerKeyboardController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKeyboardController : MonoBehaviour {
+	private Player Player;
+	// Use this for initialization
+	void Start () {
+		Player = GetComponent<Player> ();
+		if (Player == null) {
+			Debug.LogError ("Player not set to Controller");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Player != null) {
+			if (Input.GetKeyDown (KeyCode.W)) {
+
+				Player.Jump();
+
+			}else if (Input.GetKey (KeyCode.A)) {
+				
+				Player.MoveLeft ();
+
+			} else if (Input.GetKey (KeyCode.D)) {
+
+				Player.MoveRight ();
+			}
+
+			
+
+			
+
+			
+
+
+
+		}
+		
+	}
+}
